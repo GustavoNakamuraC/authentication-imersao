e8152591ae3dad4de707ff0ca2913df2e44f80b8
André Gustavo <andregustavodeoliveira49@gmail.com>
2024-10-31 21:43:53 -0300

    continuação

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Request} from '@nestjs/common';
+
 import { AuthService } from './auth.service';
 import { UserDTO } from 'src/users/DTO/users.dto';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from './auth.guard';
+import { Get } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +14,11 @@ export class AuthController {
     @Post('login')
     signIn(@Body() signInDto: UserDTO) {
       return this.authService.signIn(signInDto.name, signInDto.password);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+      return req.user;
     }
 }
